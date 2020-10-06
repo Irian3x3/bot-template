720888333325d9c792bef8061d3eda31d960da2e
Irian3x3 <68793401+Irian3x3@users.noreply.github.com>
2020-10-06 13:32:31 -0400

    Delete cmd-example.js

--- commands/cmd-example.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'example',
-	description: 'Description Example',
-	aliases: ["alias1", "alias2"],
-	execute(message, args, client) {
-		// Some code here...
-		message.channel.send('Hello world');
-	}
-};

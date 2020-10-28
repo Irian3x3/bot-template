0dbe57dfb6aa10917b07bbaac25b474c70489513
Irian3x3 <irian3x3@gmail.com>
2020-10-28 12:26:59 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client({
+	disableMentions: "everyone" // This is optional. Disables @everyone and @here pings.
+});
+
+client.config = config;
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+client.once('ready', () => {
+	client.user.setActivity("status", {
+		type: "PLAYING" // Valid fields are "PLAYING", "LISTENING", "WATCHING", and "STREAMING"
+	});
+	console.log(`Ready on login ${client.user.tag}!`);
+});
+
+
+client.on('message', message => {
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+  // This makes commands not be executable in DMs
+	if (message.channel.type === 'dm') {
+		return;
+	}
+	if (command.ownerOnly && !config.owners.includes(message.author.id)) {
+	    const nope = new Discord.MessageEmbed()
+			.setDescription("Only bot owner can execute this command!")
+			.setColor("RED")
+		return message.channel.send(nope)
+	} // Makes commands labeled as owner-only not execute unless the user is the bot owner.
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+
+    // This is for cooldowns xd
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args, client);
+	} catch (error) {
+		console.error(error);
+		message.channel.send(`There was an error executing the command \`${command.name}\`: \n \`\`\`${error}\`\`\`); // This will execute on command errors
+	}
+});
+
+
+client.login(config.token); // Logs into the bot

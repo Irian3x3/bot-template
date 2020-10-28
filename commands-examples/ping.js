7d7680560ae6235f129787135f872090bc03eee6
Irian3x3 <irian3x3@gmail.com>
2020-10-28 12:19:10 -0400

    Delete ping.js

--- commands-examples/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ping',
-	description: 'Pong!',
-	execute(message, args, client) {
-	
-       /* ping example 1 */ message.channel.send("Pong!" + message.client.ws.ping)
-       /* ping example 2 */ message.channel.send(`Pong! ${message.client.ws.ping}`)
-	}
-};
